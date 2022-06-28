38ee2d7dda650071591fe518560cc554139628d0
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-06-28 10:15:07 +0430

    yellow_Authentication_ui

---
+++ lib/style/text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const bigSizeStyle=TextStyle(color: color2,fontSize: 28,fontWeight: FontWeight.w900,);
+const blackStyle=TextStyle(color: color2,fontSize: 16,fontWeight: FontWeight.w500,);
+const greyStyle=TextStyle(color: color2,fontSize: 16);
+final hintStyle=TextStyle(color: Colors.grey[800],fontSize: 14,fontWeight: FontWeight.w500,);
+const whiteStyle=TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500,);
